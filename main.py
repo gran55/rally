8e75b0f11ca3d8ecb30f6e3f4b9ae40e292334ee
gran55 <gran55.4@yandex.ru>
2020-11-02 10:10:13 +1000

    kjk

--- main.py
+++ main.py
@@ -2,10 +2,28 @@ import pygame as pg
 
 SIZE = WIDTH, HEIGHT =800, 600 
 GREY =(128, 128, 128)
+GREEN = (0, 128, 0)
+WHITE = (200, 200, 200)
 
 pg.init()
 pg.display.set_caption('NFS Underground')
 screen = pg.display.set_mode(SIZE)
+
+FPS = 120
+clock = pg.time.Clock()
+bg_image_2_rect = bg_image.get_rect(topleft=(0, HEIGHT))
+
+
+def bg():
+    pg.draw.line(screen,  GREEN, (20, 0), (20, 600), 40)
+    pg.draw.line(screen,  GREEN, (780, 0), (780, 600), 40)
+    for xx in range(10):
+        for yy in range(10):
+            pg.draw.line(
+                screen, WHITE,
+                (40 + xx * 80, 0 if xx == 0 or xx == 9 else 10 + yy * 60), 
+                (40 + xx * 80,600 if xx == 0 or xx == 9 else 50 + yy *60), 5)
+   
 
 
 class Car(pg.sprite.Sprite):
@@ -31,6 +49,11 @@ while game:
        car3.y = HEIGHT
 
     screen.fill(GREY)
+    #bg()
     screen.blit(car3_image, (car3.x, car3.y))
+    screen.blit(bg)
     pg.display.update()
-    pg.time.wait(1)
+    clock.tick(FPS)
+    pg.display.set_caption(f'NFS Underground       FPS: {int(clock.get_fps())}')
+    
+#pg.image.save(screen, 'road.jpg')
