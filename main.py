730cd89aee344479dce536d936478c8dabf42d3a
gran55 <gran55.4@yandex.ru>
2020-11-09 09:54:23 +1000

    fgfgf

--- main.py
+++ main.py
@@ -11,8 +11,12 @@ screen = pg.display.set_mode(SIZE)
 
 FPS = 120
 clock = pg.time.Clock()
-bg_image_2_rect = bg_image.get_rect(topleft=(0, HEIGHT))
 
+bg_image = pg.image.load('Image/road.jpg')
+bg_image_rect = bg_image.get_rect(topleft=(0, 0))
+bg_image_2_rect = bg_image.get_rect(topleft=(0, -HEIGHT))
+
+print(bg_image_rect)
 
 def bg():
     pg.draw.line(screen,  GREEN, (20, 0), (20, 600), 40)
@@ -27,9 +31,24 @@ def bg():
 
 
 class Car(pg.sprite.Sprite):
-    def __init__(self):
+    def __init__(self,x= x, y,):
        pg.sprite.Sprite.__init__(self)
+
+       self.image = pg.Surface(screen.size())
+       pg.draw.line(self.image,  GREEN, (20, 0), (20, 600), 40)
+       pg.draw.line(self.image,  GREEN, (780, 0), (780, 600), 40)
+       for xx in range(10):
+           for yy in range(10):
+               pg.draw.line(
+                   self.image, WHITE,
+                   (40 + xx * 80, 0 if xx == 0 or xx == 9 else 10 + yy * 60),
+                   (40 + xx * 80, 600 if xx == 0 or xx == 9 else 60 + yy * 60), 5)
+        self.rect = self.image.get_rect(topleft=(x, y))
+   
        self.image = pg.image.load('Image/car3.png')
+       self.speed = 1
+
+all_sprite
     
 
 car3 = Car()
@@ -51,9 +70,9 @@ while game:
     screen.fill(GREY)
     #bg()
     screen.blit(car3_image, (car3.x, car3.y))
-    screen.blit(bg)
+    screen.blit(bg_image, bg_image_rect)
     pg.display.update()
     clock.tick(FPS)
-    pg.display.set_caption(f'NFS Underground       FPS: {int(clock.get_fps())}')
-    
-#pg.image.save(screen, 'road.jpg')
+    pg.display.set_caption(f"NFS Underground       FPS: {int(clock.get_fps())}")
+
+#pg.image.save(screen, 'Image/road.jpg')
